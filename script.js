31ee2fb0eabd576d7b2f06a58464add1fd83dd5e
ljones315 <ltjones2002@gmail.com>
2021-04-23 21:56:17 -0400

    Added animation for name

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+var i = 0;
+var name = "Liam Jones";
+var soFar = "";
+
+function typeWriter() {
+  if (i <= name.length) {
+    soFar = name.substring(0, i);
+    document.getElementById("name").innerHTML = soFar + "_";
+    i++;
+    setTimeout(typeWriter, 150);
+  }
+}
+
 const links = document.querySelectorAll(".scroll_to");
+
 links.forEach((item)=>{
   item.addEventListener("click", ()=>{
     const element = document.getElementById(item.getAttribute("data-link"));
